2973587436879e73f21bbec3156247a1c1bcb55a
musiccity365 <m_sackstein@yahoo.com>
2021-04-27 23:50:19 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+let num1 = 1;
+let num2 = 62;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = 9 % 5;
+
+let max = Math.max(20);
